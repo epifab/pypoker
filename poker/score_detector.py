55aa64f154b65963942a520a2d88fa777806b8db
epifab <epifab@gmail.com>
2016-10-19 21:04:56 +0000

    Score strength

--- poker/score_detector.py
+++ poker/score_detector.py
@@ -133,47 +133,15 @@ class Score:
         return self._category
 
     @property
-    def cards(self, limit=0):
+    def cards(self):
         return self._cards
 
-    def cmp(self, other):
+    @property
+    def strength(self):
         raise NotImplemented
 
-    @staticmethod
-    def _cmp_cards(cards1, cards2):
-        """Compare two list of cards according to ranks and suits."""
-        rank_diff = Score._cmp_ranks(cards1, cards2)
-        if rank_diff:
-            return rank_diff
-        return Score._cmp_suits(cards1, cards2)
-
-    @staticmethod
-    def _cmp_ranks(cards1, cards2):
-        """Compare two list of cards ranks.
-        Returns a negative integer if cards1 < cards2, positive if cards1 > cards2 or 0 if their ranks are identical"""
-        for i in range(len(cards1)):
-            try:
-                rank_diff = cards1[i].rank - cards2[i].rank
-                if rank_diff:
-                    return rank_diff
-            except IndexError:
-                # cards1 is longer than cards2
-                return 1
-        return 0 if len(cards1) == len(cards2) else -1 # cards2 is longer than cards1
-
-    @staticmethod
-    def _cmp_suits(cards1, cards2):
-        """Compare two list of cards suits.
-        Returns a negative integer if cards1 < cards2, positive if cards1 > cards2 or 0 if their suits are identical"""
-        for i in range(len(cards1)):
-            try:
-                suit_diff = cards1[i].suit - cards2[i].suit
-                if suit_diff:
-                    return suit_diff
-            except IndexError:
-                # cards1 is longer than cards2
-                return 1
-        return 0 if len(cards1) == len(cards2) else -1  # cards2 is longer than cards1
+    def cmp(self, other):
+        return cmp(self.strength, other.strength)
 
     def dto(self):
         return {
@@ -192,13 +160,25 @@ class TraditionalPokerScore(Score):
     FLUSH = 6
     QUADS = 7
     STRAIGHT_FLUSH = 8
-    
-    def cmp(self, other):
-        # Compare categories first
-        categories_diff = self.category - other.category
-        if categories_diff:
-            return categories_diff
 
+    @property
+    def strength(self):
+        strength = self.category
+        for offset in range(5):
+            strength <<= 4
+            try:
+                strength += self.cards[offset].rank
+            except IndexError:
+                pass
+        for offset in range(5):
+            strength <<= 2
+            try:
+                strength += self.cards[offset].suit
+            except IndexError:
+                pass
+        return strength
+
+    def cmp(self, other):
         # Same score, compare the list of cards
         cards1 = self.cards[0:5]
         cards2 = other.cards[0:5]
@@ -211,7 +191,7 @@ class TraditionalPokerScore(Score):
             elif TraditionalPokerScore._straight_is_min(cards1) and TraditionalPokerScore._straight_is_max(cards2):
                 return 1
 
-        return Score._cmp_cards(cards1, cards2)
+        return cmp(self.strength, other.strength)
 
     @staticmethod
     def _straight_is_min(straight_sequence):
@@ -233,17 +213,19 @@ class HoldemPokerScore(Score):
     QUADS = 7
     STRAIGHT_FLUSH = 8
 
+    @property
+    def strength(self):
+        strength = self.category
+        for offset in range(5):
+            strength <<= 4
+            try:
+                strength += self.cards[offset].rank
+            except IndexError:
+                pass
+        return strength
+
     def cmp(self, other):
-        # Compare categories first
-        categories_diff = self.category - other.category
-        if categories_diff:
-            return categories_diff
-
-        # Same score, compare the list of cards
-        cards1 = self.cards[0:5]
-        cards2 = other.cards[0:5]
-
-        return Score._cmp_ranks(cards1, cards2)
+        return cmp(self.strength, other.strength)
 
 
 class ScoreDetector:
